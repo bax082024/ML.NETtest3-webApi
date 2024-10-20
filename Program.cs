71984de642ef0d711069b336831f0da849a15080
bax082024 <bax082024@gmail.com>
2024-10-20 20:02:50 +0200

    added predictionengine

--- Program.cs
+++ Program.cs
@@ -17,6 +17,13 @@ public class ClusteringController : ControllerBase
     ITransformer trainedModel = mlContext.Model.Load("Models/customerClusteringModel.Zip", out var modelSchema);
   }
   
+  //Prediction Engine
+  _predictionEngine = MLContext.Model.CreatePredictionEngine<CustomerData, ClusterPrediction>(trainedModel);
 
-
+  [HttpPost]
+  public ActionResult<string> PredictCluster([FromBody] CustomerData customerData)
+  {
+    var prediction = _predictionEngine.Predict(customerData);
+    return Ok($"Customer belongs to cluster: {prediction.PredictedClusterId}");
+  }
 } 
